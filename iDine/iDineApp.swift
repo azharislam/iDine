cedf5a6d6c7ee6d9ac0daa8ce2f32330fbd7ddbb
Azhar Islam <azhartislam@gmail.com>
2021-03-17 14:22:01 +0000

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Azhar on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
